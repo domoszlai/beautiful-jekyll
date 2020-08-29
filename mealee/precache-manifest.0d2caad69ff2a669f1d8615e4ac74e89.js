0072d7b537290bec841e4b0ceb46cba04c332fde
domoszlai <dlacko@gmail.com>
2020-08-29 10:53:14 +0200

    Add mealee

---
+++ mealee/precache-manifest.0d2caad69ff2a669f1d8615e4ac74e89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50a8848cc55b026d5031bc0a64ba1520",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fff5bc2394aaad6bce1",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "f82da07b81759d99855f",
+    "url": "/static/css/main.c2c12290.chunk.css"
+  },
+  {
+    "revision": "3fff5bc2394aaad6bce1",
+    "url": "/static/js/2.98ccdac9.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/static/js/2.98ccdac9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f82da07b81759d99855f",
+    "url": "/static/js/main.85cbca3b.chunk.js"
+  },
+  {
+    "revision": "455b7b5527107b7e2d09",
+    "url": "/static/js/runtime-main.8853458c.js"
+  }
+]);
